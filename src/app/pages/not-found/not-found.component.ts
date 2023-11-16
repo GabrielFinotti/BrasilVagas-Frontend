063cdb6e4538809a2d842c262f05d0ebe3219675
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-11-16 11:01:57 -0300

    Coding

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent {
+
+  constructor(private router: Router) {
+    
+  }
 
 }
