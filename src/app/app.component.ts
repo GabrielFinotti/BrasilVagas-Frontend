8bce97ea75569a1bd55e47e3480ead1b71b18430
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-02-22 12:57:50 -0300

    Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,6 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'BrasilVagas';
-}
+export class AppComponent {}
